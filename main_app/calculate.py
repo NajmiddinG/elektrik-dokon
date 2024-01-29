8c1e337f75cd0480bdd9c6592bb050d2b710b09b
NajmiddinG <najmiddinweb@gmail.com>
2024-01-29 19:22:10 +0500

    23423

--- main_app/calculate.py
+++ main_app/calculate.py
@@ -11,7 +11,7 @@ def calculate_worker_to_obyekt(workdaymoney_id):
         work_amount.save()
         obyekt_data = work_amount.obyekt_set.values().first()
         # work.job.total_completed+=work.completed
-        obyekt_data['real_dept'] = obyekt_data['real_dept']-work.completed*work_amount.service_price
+        obyekt_data['real_dept'] = obyekt_data['real_dept']-work.completed*work_amount.first_price
 
         first_obyekt = work_amount.obyekt_set.first()
         for key, value in obyekt_data.items():
