735c186792e753c36bc31e9744d22be4e56cbb82
NajmiddinG <najmiddinweb@gmail.com>
2024-02-07 20:56:14 +0500

    32423423

--- dokon_app/templatetags/custom_filters.py
+++ dokon_app/templatetags/custom_filters.py
@@ -13,11 +13,11 @@ def spacecomma(value):
 @register.filter(name='real_money')
 def real_money(price, percentage):
     return spacecomma((price*(100+percentage))//100)
-    # s_text = ''
-    # money = str(value)[::-1]
-    # for i in range(len(money)//3+1):
-    #     s_text+=money[3*i:3*i+3]+' '
-    # return s_text.strip()[::-1]
+
+
+@register.filter(name='real_money2')
+def real_money2(price, percentage):
+    return (price*(100+percentage))//100
 
 @register.filter(name='bool_to_word')
 def bool_to_word(value):
